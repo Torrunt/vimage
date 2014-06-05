5624a6cba7243dfb457936c6dba95d3ca03f183a
Torrunt <me@torrunt.net>
2014-06-05 18:30:20 +0800

    Basic winform setup for vimage_settings
    Basic layout, loads values from the config. Still needs; saving, control
    bindings and context menu customisation.

---
+++ vimage_settings/Source/ConfigWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using vimage;
+
+namespace vimage_settings
+{
+    public partial class ConfigWindow : Form
+    {
+        private Config vimageConfig;
+
+        public ConfigWindow()
+        {
+            InitializeComponent();
+
+            // Load Config File
+            vimageConfig = new Config();
+            vimageConfig.Load(AppDomain.CurrentDomain.BaseDirectory + "config.txt");
+
+            // Set Values
+            checkBox_OpenAtMousePosition.Checked = vimageConfig.Setting_OpenAtMousePosition;
+            checkBox_SmoothingDefault.Checked = vimageConfig.Setting_SmoothingDefault;
+            checkBox_BackgroundForImagesWithTransparencyDefault.Checked = vimageConfig.Setting_BackgroundForImagesWithTransparencyDefault;
+            checkBox_LimitImagesToMonitorHeight.Checked = vimageConfig.Setting_LimitImagesToMonitorHeight;
+            checkBox_PositionLargeWideImagesInCorner.Checked = vimageConfig.Setting_PositionLargeWideImagesInCorner;
+            checkBox_PreloadNextImage.Checked = vimageConfig.Setting_PreloadNextImage;
+            numericUpDown_MinImageSize.Value = vimageConfig.Setting_MinImageSize;
+            numericUpDown_SmoothingMinImageSize.Value = vimageConfig.Setting_SmoothingMinImageSize;
+
+            checkBox_ContextMenuShowMargin.Checked = vimageConfig.Setting_ContextMenuShowMargin;
+        }
+
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
